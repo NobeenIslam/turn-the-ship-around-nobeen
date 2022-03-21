674f0c0c685d30ea856cfc64ba5e8dbe5794c423
Nobeen <n.islam.1@outlook.com>
2022-03-21 16:51:26 +0000

    Created successful tests for isSearchTermPresent without case-sensitivity

---
+++ src/utils/isSearchTermPresent.test.ts
@@ -0,0 +1,9 @@
+import episodes from "../episodes.json";
+import  isSearchTermPresent  from "./isSearchTermPresent";
+
+test("greet returns a string, greeting the passed name", () => {
+  expect(isSearchTermPresent(episodes[0],"bastard son")).toBe(true);
+  expect(isSearchTermPresent(episodes[1],"bastard son")).toBe(false);
+  expect(isSearchTermPresent(episodes[2],"bastard son")).toBe(false);
+  expect(isSearchTermPresent(episodes[9],"finds himself in an unfamiliar")).toBe(true);
+});
