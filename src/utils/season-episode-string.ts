e0fe10e9535df21d2a670496a82b7674dd563428
matbat10 <97300316+matbatten@users.noreply.github.com>
2022-03-21 11:54:30 +0000

    added succesful testing code for season-episode-string

--- src/utils/season-episode-string.ts
+++ src/utils/season-episode-string.ts
@@ -2,8 +2,7 @@ import { IEpisode } from "../components/IEpisode";
 
 export function seasonEpisodeString(singleEpisode: IEpisode): string {
   // return `S${singleEpisode.season}E${singleEpisode.number}`;
-  return `S${ String(singleEpisode.season).padStart(2, '0')}
-  E${ String(singleEpisode.number).padStart(2, '0')}`;
+  return `S${ String(singleEpisode.season).padStart(2, '0')}E${ String(singleEpisode.number).padStart(2, '0')}`;
 }
 
 
