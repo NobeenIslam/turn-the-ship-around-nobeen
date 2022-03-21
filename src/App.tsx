3485f66951222cae9f1e8110aaa307ac063346ce
matbat10 <97300316+matbatten@users.noreply.github.com>
2022-03-21 11:01:29 +0000

    imported episodes to App and test lines of code to check working

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import { greet } from "./utils/greet";
+import episodes from "./episodes.json";
+
+// test episodes pulling correctly-
+// console.log(`Imported ${episodes.length} episode(s)`);
+// console.log(`First episode's name is ${episodes[0].name}`);
 
 function App(): JSX.Element {
   return <h1>{greet("World")}</h1>;
